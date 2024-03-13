ef2ce1e30de4ac8f6206c0d0165b05671fdb9293
Andres435 <andresfqe09@gmail.com>
2024-03-12 19:43:51 -0700

    User Login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'REFT Home Page',
+};
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello REFT bois</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-5xl max-[500px]:text-2xl">Hello REFT bois</h1>
     </main>
   );
 }
