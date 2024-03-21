2b134e022216c54ad953a3e629fc829fc634ba9c
Michael Carey <mpcarey@csu.fullerton.edu>
2024-03-20 19:30:07 -0700

    REFT-130 updated all the auth forms

--- frontend/app/ui/Nav.tsx
+++ frontend/app/ui/Nav.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Navbar,
   NavbarBrand,
@@ -5,10 +6,22 @@ import {
   NavbarLink,
   NavbarToggle,
 } from "flowbite-react";
-import ThemeSwitch from "./ThemeSwitch";
+import { useState } from "react";
+import Link from "next/link";
+import Button from "./Button";
 import UserDropdown from "./UserDropdown";
 
 function Nav() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <Navbar fluid rounded>
       <NavbarBrand href="/">
@@ -16,20 +29,32 @@ function Nav() {
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           REFT
         </span>
-        <ThemeSwitch />
       </NavbarBrand>
       <div className="flex md:order-2">
-        <UserDropdown />
+        {isLoggedIn ? (
+          <div>
+            <UserDropdown />
+          </div>
+        ) : (
+          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+            <div className="flex py-2 px-3 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">
+              <Link href="/auth/signin" onClick={handleLogin}>
+                Sign in
+              </Link>
+            </div>
+            <div className="ml-4">
+              <Button href="/auth/signup" text="Signup" />
+            </div>
+          </div>
+        )}
         <NavbarToggle />
       </div>
       <NavbarCollapse>
-        <NavbarLink href="#" active>
-          Home
-        </NavbarLink>
         <NavbarLink href="#">Thing 1</NavbarLink>
         <NavbarLink href="#">Thing 2</NavbarLink>
         <NavbarLink href="#">Thing 3</NavbarLink>
         <NavbarLink href="#">Thing 4</NavbarLink>
+        <NavbarLink href="#">Thing 5</NavbarLink>
       </NavbarCollapse>
     </Navbar>
   );
--- frontend/app/ui/UserDropdown.tsx
+++ frontend/app/ui/UserDropdown.tsx
@@ -1,37 +1,77 @@
-import {
-  Avatar,
-  Dropdown,
-  DropdownDivider,
-  DropdownHeader,
-  DropdownItem,
-} from "flowbite-react";
+import React, { useState } from "react";
+import Link from "next/link";
 
 const UserDropdown = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setDropdownOpen(false);
+  };
+
   return (
-    <div>
-      <Dropdown
-        arrowIcon={false}
-        inline
-        label={
-          <Avatar
-            alt="User settings"
-            img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-            rounded
-          />
-        }
-      >
-        <DropdownHeader>
-          <span className="block text-sm">Username</span>
-          <span className="block truncate text-sm font-medium">
-            name@email.com
-          </span>
-        </DropdownHeader>
-        <DropdownItem>Tokens</DropdownItem>
-        <DropdownItem>Wallet</DropdownItem>
-        <DropdownItem>Settings</DropdownItem>
-        <DropdownDivider />
-        <DropdownItem>Sign out</DropdownItem>
-      </Dropdown>
+    <div className="relative inline-block">
+      <img
+        id="avatarButton"
+        onClick={toggleDropdown}
+        className="w-10 h-10 rounded-full cursor-pointer hover:ring-2 ring-gray-300 dark:ring-gray-500"
+        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        alt="User dropdown"
+      />
+
+      {/* Dropdown menu */}
+      {dropdownOpen && (
+        <div className="absolute z-10 right-0 mt-2 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+          <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+            <div>Reft Boi</div>
+            <div className="font-medium truncate">Boi@REFT.com</div>
+          </div>
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-gray-200"
+            aria-labelledby="avatarButton"
+          >
+            <li>
+              <Link
+                href="#"
+                onClick={closeDropdown}
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Token Management
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                onClick={closeDropdown}
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Something
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                onClick={closeDropdown}
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Settings
+              </Link>
+            </li>
+          </ul>
+          <div className="py-1">
+            <Link
+              href="/auth/signout"
+              onClick={closeDropdown}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            >
+              Sign out
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
