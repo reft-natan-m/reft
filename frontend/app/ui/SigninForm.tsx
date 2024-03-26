14ca48c5f308a9fea3e1ad7af2cd61e6bb81cda8
Alejandro Guerrero <Gu_Alejandro.007@csu.fullerton.edu>
2024-03-26 08:40:28 -0700

    Fixed login button on sign in form
    Fixed the submit button so it says login instead of create account

--- frontend/app/ui/SigninForm.tsx
+++ frontend/app/ui/SigninForm.tsx
@@ -96,7 +96,7 @@ function SigninForm() {
         </div>
         <input
           type="submit"
-          value="Create your Account"
+          value="Login"
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         />
         <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
