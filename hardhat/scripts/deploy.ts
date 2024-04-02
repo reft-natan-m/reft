8d8a361643834aa9b79f9e945801325af724259c
NADEE-MJ <nadeem.maida@gmail.com>
2024-04-02 13:04:35 -0700

    REFT-148 cleaned up deploy.ts

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,13 +1,26 @@
 import { ethers } from "hardhat";
+import { Signer } from "ethers";
+
+async function getAccountBalance(account: Signer) {
+  return ethers.utils.formatEther(await account.getBalance());
+}
 
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log(
+    `Deploying contracts with the following account address: ${deployer.address}`
+  );
+  console.log(
+    `Deployer Account Balance: ${await getAccountBalance(deployer)} ETH`
+  );
 
   const token = await ethers.deployContract("RealEstateFungibleToken");
 
-  console.log("Token address:", await token.address);
+  console.log(`Token deployed to address: ${token.address}`);
+  console.log(
+    `Deployer Account Balance: ${await getAccountBalance(deployer)} ETH`
+  );
 }
 
 main()
