f0d7e8c2013ef65737a8035a85376cdbcbb94236
Michael Carey <mpcarey@csu.fullerton.edu>
2024-04-02 21:21:35 -0700

    REFT-151 More shenanigans

--- frontend/app/ui/UserDropdown.tsx
+++ frontend/app/ui/UserDropdown.tsx
@@ -1,8 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
-const UserDropdown = () => {
+const UserDropdown: React.FC = () => {
+  const { data: session } = useSession();
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [avatarURL, setAvatarURL] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`/api/register?email=${session?.user?.email}`);
+        if (!res.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        const userData = await res.json();
+        setAvatarURL(userData.avatar);
+      } catch (error: any) {
+        setErrorMessage(error.message);
+      }
+    };
+
+    fetchData();
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [session]);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -13,12 +49,15 @@ const UserDropdown = () => {
   };
 
   return (
-    <div className="relative inline-block">
+    <div className="relative inline-block" ref={dropdownRef}>
       <img
         id="avatarButton"
         onClick={toggleDropdown}
         className="w-10 h-10 rounded-full cursor-pointer hover:ring-2 ring-gray-300 dark:ring-gray-500"
-        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        src={
+          avatarURL ||
+          "https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        }
         alt="User dropdown"
       />
 
@@ -33,33 +72,7 @@ const UserDropdown = () => {
             className="py-2 text-sm text-gray-700 dark:text-gray-200"
             aria-labelledby="avatarButton"
           >
-            <li>
-              <Link
-                href="#"
-                onClick={closeDropdown}
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Token Management
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/wallet"
-                onClick={closeDropdown}
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Wallet
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/user/0/settings"
-                onClick={closeDropdown}
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Settings
-              </Link>
-            </li>
+            {/* Links */}
           </ul>
           <div className="py-1">
             <Link
