ccf4fd181b858ef93cc8a8f88596f1a102ec833c
Michael Carey <mpcarey@csu.fullerton.edu>
2024-04-02 23:48:46 -0700

    REFT-150 :)

--- frontend/app/ui/UserDropdown.tsx
+++ frontend/app/ui/UserDropdown.tsx
@@ -6,30 +6,9 @@ const UserDropdown: React.FC = () => {
   const { data: session } = useSession();
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
-  const [avatarURL, setAvatarURL] = useState<string>("");
-  const [username, setUsername] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(`/api/user?email=${session?.user?.email}`);
-
-        if (!res.ok) {
-          throw new Error("Failed to fetch user data");
-        }
-        const userData = await res.json();
-        setAvatarURL(userData.avatar);
-
-        setUsername(userData.username);
-
-      } catch (error: any) {
-        setErrorMessage(error.message);
-      }
-    };
-
-    fetchData();
-
     const handleClickOutside = (event: MouseEvent) => {
       if (
         dropdownRef.current &&
@@ -60,7 +39,7 @@ const UserDropdown: React.FC = () => {
         onClick={toggleDropdown}
         className="w-10 h-10 rounded-full cursor-pointer hover:ring-2 ring-gray-300 dark:ring-gray-500"
         src={
-          avatarURL ||
+          session?.user?.image ||
           "https://flowbite.com/docs/images/people/profile-picture-5.jpg"
         }
         alt="User dropdown"
@@ -70,7 +49,7 @@ const UserDropdown: React.FC = () => {
       {dropdownOpen && (
         <div className="absolute z-10 right-0 mt-2 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
           <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-            <div>{username}</div>
+            <div>{session?.user?.name}</div>
             <div className="font-medium truncate">{session?.user?.email}</div>
           </div>
           <ul
@@ -97,7 +76,7 @@ const UserDropdown: React.FC = () => {
             </li>
             <li>
               <Link
-                href={`/user/${username}/settings/email`}
+                href={`/user/${session?.user?.name}/settings/email`}
                 onClick={closeDropdown}
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
