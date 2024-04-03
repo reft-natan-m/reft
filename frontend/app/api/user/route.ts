ad1bd84dc90f2e779453902a14c9aaed7196a854
Michael Carey <mpcarey@csu.fullerton.edu>
2024-04-02 21:26:32 -0700

    REFT-150 get that GET

---
+++ frontend/app/api/user/route.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const email = searchParams.get("email");
+
+  if (!email) {
+    return new NextResponse("Missing email", {
+      status: 400,
+    });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    return new NextResponse("No user found", {
+      status: 404,
+    });
+  }
+
+  return NextResponse.json(user);
+}
