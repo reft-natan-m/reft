55c4b3d064631cc633f03494832a4655506907ef
Nathan Storm <105747854+theStorm1995@users.noreply.github.com>
2024-04-14 23:24:58 -0700

    REFT-145 Finised up sell listing endpoint

---
+++ frontend/app/api/listing/create/route.ts
@@ -0,0 +1,67 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { userId, propertyId, tokens } = body;
+
+  try {
+    const prismaUser = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { tokens: true },
+    });
+
+    if (!prismaUser) {
+      return new NextResponse("User doesnt exist", { status: 400 });
+    }
+
+    const tokenToSplit = prismaUser.tokens.map((token) => {
+      if (token.propertyId === propertyId) {
+        return token;
+      }
+    });
+
+    if (!tokenToSplit[0]) {
+      return new NextResponse("User doesnt own any tokens for this property", {
+        status: 400,
+      });
+    }
+
+    if (tokenToSplit[0].numberOfTokens < tokens) {
+      return new NextResponse("User doesnt own enough tokens", { status: 400 });
+    }
+
+    const newToken = await prisma.token.create({
+      data: {
+        user: { connect: { id: userId } },
+        property: { connect: { id: propertyId } },
+        numberOfTokens: tokens,
+        listed: true,
+        dateListed: new Date(),
+      },
+    });
+
+    await prisma.token.update({
+      where: {
+        id: tokenToSplit[0].id,
+      },
+      data: {
+        numberOfTokens: tokenToSplit[0].numberOfTokens - tokens,
+      },
+    });
+
+    await prisma.property.update({
+      where: {
+        id: propertyId,
+      },
+      data: {
+        tokensforSale: { increment: tokens },
+      },
+    });
+
+    return NextResponse.json(newToken);
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Failed to create listing", { status: 500 });
+  }
+}
--- frontend/app/api/property/create/route.ts
+++ frontend/app/api/property/create/route.ts
@@ -1,5 +1,4 @@
 import prisma from "@/lib/prisma";
-import { connect } from "http2";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
